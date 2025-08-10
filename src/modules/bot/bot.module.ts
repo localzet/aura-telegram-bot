be5fc89616c4c353f4ee1c4da71d577bcc61fd76
Ivan Zorin <creator@localzet.com>
2025-08-10 18:16:48 +0300

    Add catcher

--- src/modules/bot/bot.module.ts
+++ src/modules/bot/bot.module.ts
@@ -18,6 +18,19 @@ const log = debug('bot:bot.module')
 export class BotModule {
     constructor(@InjectBot(BotName) private readonly bot: Bot<Context>) {
         this.bot.use(ResponseTime);
+        this.bot.catch((err) => {
+              const ctx = err.ctx;
+                console.error(`Error while handling update ${ctx.update.update_id}:`);
+                  const e = err.error;
+                    if (e instanceof GrammyError) {
+                        console.error("Error in request:", e.description);
+                          } else if (e instanceof HttpError) {
+                              console.error("Could not contact Telegram:", e);
+                                } else {
+                                    console.error("Unknown error:", e);
+                                      }
+                                      });
+        })
         log('EchoService starting ')
     }
 }
